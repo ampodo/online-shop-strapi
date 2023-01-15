1814eeaf52542852d96730ed1fd0d6fc36678321
Vladimir Skrjabin <skrjabingrail@gmail.com>
2023-01-15 19:40:08 +0200

    homepage category added

--- src/components/Home/Category/Category.jsx
+++ src/components/Home/Category/Category.jsx
@@ -1,6 +1,28 @@
 import "./Category.scss";
+import derm1 from "../../../assets/category/derm1.jpg";
+import hydra2 from "../../../assets/category/hydra2.jpg";
+import men3 from "../../../assets/category/men3.jpg";
+import sevemiracle from "../../../assets/category/sevemiracle.jpg";
+
 const Category = () => {
-    return <div>Category</div>;
+  return (
+    <div className="category-shop">
+      <div className="categories">
+        <div className="category">
+          <img src={derm1} alt="" />
+        </div>
+        <div className="category">
+          <img src={hydra2} alt="" />
+        </div>
+        <div className="category">
+          <img src={men3} alt="" />
+        </div>
+        <div className="category">
+          <img src={sevemiracle} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Category;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,19 @@
 import "./Home.scss";
 
 import Banner from "./Banner/Banner"
-
+import Category from "./Category/Category";
 
 const Home = () => {
     return (
     
     
-    <div className="home">
+    <div>
                  <Banner />
+                     <div className="main-content">
+                     <div className="layout">
+                   <Category />
+                   </div>
+                   </div>
      </div>
 
     );
