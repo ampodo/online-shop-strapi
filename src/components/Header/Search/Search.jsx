b3fed7e8bbb213f9ad05ddc2505e82b0bd88fd90
Vladimir Skrjabin <skrjabingrail@gmail.com>
2023-03-06 22:10:31 +0200

    search modal functionality added

--- src/components/Header/Search/Search.jsx
+++ src/components/Header/Search/Search.jsx
@@ -1,32 +1,65 @@
 import React, { useState } from "react";
 import "./Search.scss";
 import { MdClose } from "react-icons/md";
-import prod from "../../../assets/products/miracle1.png";
+import useFetch from "../../../hooks/useFetch";
+import { useNavigate } from "react-router-dom";
+
 
 const Search = ({ setShowSearch }) => {
+
+
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+      setQuery(e.target.value);
+  };
+
+  let { data } = useFetch(`/api/products?populate=*&filters[title][$contains]=${query}`);
+
+  if (!query.length) {
+    data = null; }
+
   return (
     <div className="search-modal">
       <div className="form-field">
-        <input type="text" autoFocus placeholder="Search" />
+        <input type="text" 
+         autoFocus 
+         placeholder="Search"
+         value={query} 
+         onChange={onChange}
+
+        />
 
         <MdClose onClick={() => setShowSearch(false)} />
       </div>
 
       <div className="search-result-content">
         <div className="search-results">
-          <div className="search-result-item">
-            <div className="img-container">
-              <img src={prod} alt="" />
-            </div>
+              {data?.data?.map(item => (
+                  <div className="search-result-item"    
+                       key={item.id}
+                       onClick={() => {
+                                navigate("/product/" + item.id);
+                                setShowSearch(false);
+                            }}  
+                  >
+                  <div className="img-container">
 
-            <div className="prod-details">
-              <span className="name">product name</span>
-              <span className="desc">product desc</span>
+                  <img src={ process.env.REACT_APP_DEV_URL +
+                   item.attributes.img.data[0].attributes.url} alt="" />
+                  </div>
+      
+                  <div className="prod-details">
+                    <span className="name">{item.attributes.title}</span>
+                    <span className="desc">{item.attributes.desc}</span>
+                  </div>
+             </div>
+
+                 ))}
             </div>
-          </div>
-        </div>
-      </div>
-    </div>
+         </div>
+     </div>
   );
 };
 
